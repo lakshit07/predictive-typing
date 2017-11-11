f5a230c5c643e02a66424f2bee8e23f074914394
anirudhbansal <anirudhbansal95@gmail.com>
2017-11-11 13:49:06 +0530

    Evaluation of methods added

--- smoothing/driver.py
+++ smoothing/driver.py
@@ -1,8 +1,30 @@
 from methods import *
+from evaluation import *
 import os
 
 def main():
 	print(compute('turing'))
+	testfile = './test/test.txt'
+	print ('Additive smoothing')
+	(perplexity,cross_entropy) = evaluation(testfile,'additive')
+	print ('Perplexity is ',perplexity,' and cross entropy is ',cross_entropy)
+
+	print ('Good Turing')
+	(perplexity,cross_entropy) = evaluation(testfile,'turing')
+	print ('Perplexity is ',perplexity,' and cross entropy is ',cross_entropy)
+
+	print ('Jelinek-mercer')
+	(perplexity,cross_entropy) = evaluation(testfile,'jelinek mercer')
+	print ('Perplexity is ',perplexity,' and cross entropy is ',cross_entropy)
+
+	print ('Witten Bell')
+	(perplexity,cross_entropy) = evaluation(testfile,'witten bell')
+	print ('Perplexity is ',perplexity,' and cross entropy is ',cross_entropy)
+
+	print ('Absolute Discounting')
+	(perplexity,cross_entropy) = evaluation(testfile,'absolute discounting')
+	print ('Perplexity is ',perplexity,' and cross entropy is ',cross_entropy)
+
 
 if __name__ == "__main__":
     main()	
