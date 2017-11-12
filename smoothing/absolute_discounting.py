d9d5a1992351cb44fc9bba8a4195d46a0ab36419
anirudhbansal <anirudhbansal95@gmail.com>
2017-11-12 10:08:01 +0530

    Hnadled divsion by zero in smoothing and returns evaluation metric values as

--- smoothing/absolute_discounting.py
+++ smoothing/absolute_discounting.py
@@ -9,13 +9,19 @@ def absolute_discounting(prev_words, cur_word):
         return max(unigram[cur_word] - D, 0)/config.total_unigrams
 
     if len(prev_words) == 1:
+        den = unigram[prev_words[0]]
+        if den == 0:
+            return 0
         const = (D/unigram[prev_words[0]]) * count_distinct_uni[prev_words[0]]
         num = max(bigram[prev_words[0] + ' ' + cur_word] - D, 0)
-        den = unigram[prev_words[0]]
     else:
+        den = bigram[prev_words[0] + ' ' + prev_words[1]]
+        if den == 0:
+            return 0
         const = (D / bigram[prev_words[0] + ' ' + prev_words[1]]) * count_distinct_bi[prev_words[0] + ' ' + prev_words[1]]
         num = max(trigram[prev_words[0] + ' ' + prev_words[1] + ' ' + cur_word] - D, 0)
-        den = bigram[prev_words[0] + ' ' + prev_words[1]]
 
-    ans = float(num)/den + const * absolute_discounting(prev_words[1:], cur_word)
+    ans = const * absolute_discounting(prev_words[1:], cur_word)
+    if den != 0:
+        ans += float(num)/den
     return ans
--- smoothing/evaluation.py
+++ smoothing/evaluation.py
@@ -1,4 +1,4 @@
-import re
+import re,os,json
 from absolute_discounting import *
 from additive_smoothen import *
 from good_turing import *
@@ -6,50 +6,89 @@ from jelinek_mercer import *
 from witten_bell import *
 from math import log
 
-def evaluation(testfile,method):
-	f = open(testfile,'r')
-	total_words = 0
-	cross_entropy = 0.0
-	for sentence in f:
-		split_sentence = re.split(regexPattern,sentence)
-		prev_word = prev2_word = ''
-		for word in split_sentence:
-			if word=='':
-				continue
-			total_words += 1
-			if prev_word == '':
-				cross_entropy += log(cond_prob([],word),2)
-			elif prev2_word == '':
-				cross_entropy += log(cond_prob([prev_word],word),2)
-			elif method == 'additive':
-				cross_entropy += log(additive_smoothen([prev2_word,prev_word],word),2)
-			elif method == 'turing':
-				val1 = good_turing([prev2_word,prev_word],word)
-				if val1 != 0: 
-					cross_entropy += log(val1,2)
-				else:
-					val2 = cond_prob([prev2_word,prev_word],word)
-					if val2 != 0:
-						cross_entropy += log(val2,2)
+def evaluation(testfolder,method):
+	default_p = 1.0/config.total_unigrams
+	perp = []
+	c_entropy = []
+
+	for testfile in os.listdir(testfolder):
+		f = open(testfolder+'/'+testfile,'r')
+		total_words = 0
+		cross_entropy = 0.0
+		for sentence in f:
+			split_sentence = re.split(regexPattern,sentence)
+			prev_word = prev2_word = ''
+			for word in split_sentence:
+				if word=='':
+					continue
+				total_words += 1
+				if prev_word == '':
+					p = cond_prob([],word)
+					if p==0:
+						p = default_p
+					cross_entropy += log(p,2)
+				elif prev2_word == '':
+					p = cond_prob([prev_word],word)
+					if p==0:
+						p = default_p
+					cross_entropy += log(p,2)
+				elif method == 'additive':
+					p = additive_smoothen([prev2_word,prev_word],word)
+					if p==0:
+						p = default_p
+					cross_entropy += log(p,2)
+				elif method == 'turing':
+					val1 = good_turing([prev2_word,prev_word],word)
+					if val1 != 0: 
+						cross_entropy += log(val1,2)
 					else:
-						val3 = cond_prob([prev_word],word)
-						if val3 != 0:
-							cross_entropy += log(val3,2)
+						val2 = cond_prob([prev2_word,prev_word],word)
+						if val2 != 0:
+							cross_entropy += log(val2,2)
 						else:
-							cross_entropy += log(cond_prob([],word),2)
+							val3 = cond_prob([prev_word],word)
+							if val3 != 0:
+								cross_entropy += log(val3,2)
+							else:
+								p = cond_prob([],word)
+								if p==0:
+									p = default_p
+								cross_entropy += log(p,2)
 
-			elif method == 'jelinek mercer':
-				cross_entropy += log(jelinek_mercer([prev2_word,prev_word],word),2)
-			elif method == 'witten bell':
-				cross_entropy += log(witten_bell([prev2_word,prev_word],word),2)
-			elif method == 'absolute discounting':
-				cross_entropy += log(absolute_discounting([prev2_word,prev_word],word),2)
+				elif method == 'jelinek mercer':
+					p = jelinek_mercer([prev2_word,prev_word],word)
+					if p==0:
+						p = default_p
+					cross_entropy += log(p,2)
+				elif method == 'witten bell':
+					p = witten_bell([prev2_word,prev_word],word)
+					if p==0:
+						p = default_p
+					cross_entropy += log(p,2)
+				elif method == 'absolute discounting':
+					p = absolute_discounting([prev2_word,prev_word],word)
+					if p==0:
+						p = default_p
+					cross_entropy += log(p,2)
 
-			prev2_word = prev_word
-			prev_word = word
+				prev2_word = prev_word
+				prev_word = word
 
-	cross_entropy /= (-total_words)
-	perplexity = 2.0 ** cross_entropy
-	return (perplexity,cross_entropy)
+		cross_entropy /= (-total_words)
+		perplexity = 2.0 ** cross_entropy
+		perp.append(perplexity)
+		c_entropy.append(cross_entropy)
+		f.close()
 
-	f.close()
+	return (perp,c_entropy)
+
+
+def evaluation_all(foldername):
+	(perp1,c_entropy1) = evaluation(foldername,'additive')
+	(perp2,c_entropy2) = evaluation(foldername,'turing')
+	(perp3,c_entropy3) = evaluation(foldername,'jelinek mercer')
+	(perp4,c_entropy4) = evaluation(foldername,'witten bell')
+	(perp5,c_entropy5) = evaluation(foldername,'absolute discounting')
+	c_entropy_all = [c_entropy1,c_entropy2,c_entropy3,c_entropy4,c_entropy5]
+	perp_all = [perp1,perp2,perp3,perp4,perp5]
+	return (c_entropy_all,perp_all) 
